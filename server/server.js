13ebd294b87bd0db1fc1ab8f0334e435319c21bc
arjun055 <nagarjunarjun711@gmail.com>
2024-11-21 18:41:22 +0530

    all project files added

--- server/server.js
+++ server/server.js
@@ -14,7 +14,38 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(join(__dirname,'../client')));
 
 //get routes
-
-
+app.get('/',(req,res)=>{
+    res.sendFile(join(__dirname,'../client','index.html'));
+})
 
 //post routes
+app.post('/joinRoom',(req,res)=>{
+    const {username,roomID} = req.body;
+
+    if(!username || !roomID)
+    {
+        return res.status(400).send("Both username and room ID are required");
+    }
+
+    res.redirect(`/client.html?username=${encodeURIComponent(username)}&roomID=${encodeURIComponent(roomID)}`);
+})
+
+io.on('connection',(socket)=>{
+    console.log(`${socket.id} connected`);
+
+    socket.on('joinRoom',(username,roomID)=>{
+        socket.join(roomID);
+    })
+
+    socket.on('chatMessage',(username,roomID,msg)=>{
+        socket.to(roomID).emit('chatMessage',username,msg);
+    })
+
+    socket.on('disconnect',()=>{
+        console.log(`${socket.id} is disconnected`);
+    })
+})
+
+server.listen(3000,()=>{
+    console.log("your server is running on port 3000...");
+})
